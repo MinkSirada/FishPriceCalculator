2d6a19cfd6f8ede81439409fa24e1ab87a158412
Zalmonella <156534160+MinkSirada@users.noreply.github.com>
2024-01-31 12:06:04 +0700

    Cursor in the input box after calculation

--- GUI-calculator.py
+++ GUI-calculator.py
@@ -7,6 +7,8 @@ GUI.title('Fish Price Calculation Program for Pumpuang Truck')
 GUI.geometry('500x300')
 
 bg = PhotoImage(file='Truck-icon.png')
+#bg = PhotoImage(file='C:\\Users\\mamak\\Desktop\\Basic Python\\EP5\\Truck-icon.png')
+#bg = PhotoImage(file=r'C:\Users\mamak\Desktop\Basic Python\EP5\Truck-icon.png')
 
 BG = Label(GUI, image=bg)
 BG.pack()
@@ -24,9 +26,11 @@ def Cal():
         calc = quan * 39 # 39 Baht/kg
         messagebox.showinfo('Total price','Total fish price is {} baht'.format(calc))
         v_quantity.set('')
+        E1.focus()
     except: 
         messagebox.showwarning('Wrong input', 'Please fill in only numbers')
         v_quantity.set('')
+        E1.focus()
 
 B = ttk.Button(GUI,text='Calculate',command=Cal)
 B.pack(ipadx=30, ipady=20, pady=20) #ipadx/y increase width x/y
