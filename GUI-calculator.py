3c1609184ca8c38b3b5067453b91fcb7e2ce5b6d
Zalmonella <156534160+MinkSirada@users.noreply.github.com>
2024-01-31 11:55:40 +0700

    Simple fish price calculator

---
+++ GUI-calculator.py
@@ -0,0 +1,34 @@
+
+from tkinter import *
+from tkinter import ttk, messagebox
+
+GUI = Tk()
+GUI.title('Fish Price Calculation Program for Pumpuang Truck')
+GUI.geometry('500x300')
+
+bg = PhotoImage(file='Truck-icon.png')
+
+BG = Label(GUI, image=bg)
+BG.pack()
+
+L = Label(GUI,text='Please fill in amount of fish in kg', font=(None,20))
+L.pack()
+
+v_quantity = StringVar() #Variable from the input text
+E1 = ttk.Entry(GUI, textvariable=v_quantity, font = (None,20))
+E1.pack()
+
+def Cal():
+    try:  
+        quan = float(v_quantity.get())
+        calc = quan * 39 # 39 Baht/kg
+        messagebox.showinfo('Total price','Total fish price is {} baht'.format(calc))
+        v_quantity.set('')
+    except: 
+        messagebox.showwarning('Wrong input', 'Please fill in only numbers')
+        v_quantity.set('')
+
+B = ttk.Button(GUI,text='Calculate',command=Cal)
+B.pack(ipadx=30, ipady=20, pady=20) #ipadx/y increase width x/y
+
+GUI.mainloop() #for continuous running
